c044150d233eff8cec0d57a7b988558d5b9b382b
n1k1.ss <id9295344026@gmail.com>
2025-10-18 00:33:30 -0400

    Inline mode web_site update

--- app/handlers/inline_mode.py
+++ app/handlers/inline_mode.py
@@ -4,10 +4,9 @@ from aiogram.enums import ParseMode
 def register_inline_mode_handler(dp):
     @dp.inline_query()
     async def inline_echo(inline_query: InlineQuery):
-        text = inline_query.query or "Ты ничего не написал"
-        item = InlineQueryResultArticle(
+        web_site = InlineQueryResultArticle(
             id="1",
             title="Сайт",
             input_message_content=InputTextMessageContent(message_text=f"<a href='https://inaam.ru'><b>Сайт INAAM</b></a>", parse_mode=ParseMode.HTML)
         )
-        await inline_query.answer(results=[item], cache_time=1)
+        await inline_query.answer(results=[web_site], cache_time=1)
